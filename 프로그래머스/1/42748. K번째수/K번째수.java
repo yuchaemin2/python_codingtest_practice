2fb4ede4655e7d595e5e98e54ae179c2e13e1a39
Chaemin Yu <101326708+yuchaemin2@users.noreply.github.com>
2026-05-14 11:40:51 +0900

    [level 1] Title: K번째수, Time: 0.31 ms, Memory: 61 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        int x=0;
+        for(int i=0; i<commands.length; i++)
+        {
+            int[] arr = new int[commands[i][1] - commands[i][0] + 1];
+            for(int j=commands[i][0]-1; j<commands[i][1]; j++)
+            {
+                arr[x++] = array[j];
+            }
+            Arrays.sort(arr);
+            answer[i] = arr[commands[i][2]-1];
+            x=0;
+        }
+        return answer;
+    }
+}
